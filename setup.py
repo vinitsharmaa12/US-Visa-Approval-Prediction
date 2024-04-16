894522eadc202c9650cdfe58266bff43be3ff79b
vinit sharma <vinitsharmaa1205@gmail.com>
2024-04-16 18:11:52 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="vinit",
+    author_email="vinitsharmaa1205@gmail.com",
+    packages=find_packages()
+)
